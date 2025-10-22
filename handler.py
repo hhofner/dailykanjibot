ef4f7b5aef620050c9b2d091b5e9ef40b60c980f
Hans Hofner <hhofner@gmail.com>
2025-10-22 15:11:17 +0900

    new prompt, fix alt text, add response, kun and on

--- handler.py
+++ handler.py
@@ -14,7 +14,7 @@ logging.basicConfig(
     format="%(name)s - %(levelname)s - %(message)s"
 )
 logger.setLevel(logging.INFO)
-# fileHandler = logging.FileHandler("logs.txt")
+fileHandler = logging.FileHandler("logs.txt")
 logger.addHandler(fileHandler)
 
 
@@ -48,19 +48,21 @@ def run(event, context):
     client = OpenAI(api_key=api_key)
     
     role_msg = """
-    You are a master assistant of Japanese Kanji characters.
+    You are a master of Japanese Kanji characters. You know tons
+    of fun and engaging facts about Kanji characters that entertains
+    Japanese learners and non-learners alike. You know facts about
+    Kanji characters, their history, etymology, and use in modern
+    media, books, internet slang, and other presesence.
     You talk and respond with concise and simple, yet engaging
     answers about Japanese characters. You only reply with
-    normal text and no markdown.
+    normal text and no markdown. 
     """
     
     user_msg = f"""
-    Please list three words that use the character {unicode_char}, seperated by
-    commas, in simplified manner without any other text about
-    the words used, like this: Example Words: 新聞(しんぶん)、新しい(あたらしい)、新札(しんさつ)
     Please give me an interesting fact about the kanji character
     {unicode_char}, and don't write "interesting fact", just write the
-    fact, and briefly mention modern day usage of the character.
+    fact, and briefly mention modern day usage of the character. Make sure
+    its a fun and engaging fact.
     """
     max_completion_tokens = 500
     response = None
@@ -88,7 +90,19 @@ def run(event, context):
     
     logger.info(f"Retry attemps: {retry_attempts}")
     
-    content = f"Today's Kanji: {unicode_char}\n\n" + response.choices[0].message.content + f"\n\nhttps://jisho.org/search/{unicode_char}%23kanji"
+    KANJI_API_URL = 'https://kanjiapi.dev/v1/kanji/'
+    KANJI_API_WORDS_URL = 'https://kanjiapi.dev/v1/words/'
+    
+    kanji_data_raw = requests.get(KANJI_API_URL+unicode_char)
+    kanji_data = kanji_data_raw.json()
+    logger.info(f"Kanji data: {kanji_data}")
+    
+    jisho_link = f"\n\nhttps://jisho.org/search/{unicode_char}%23kanji"
+    onyomi = f"音: {', '.join(kanji_data['on_readings'])}\n" if kanji_data['on_readings'] else ""
+    kunyomi = f"訓: {', '.join(kanji_data['kun_readings'])}\n" if kanji_data['kun_readings'] else ""
+    
+    content = f"【{unicode_char}】\n\n" + onyomi + kunyomi + response.choices[0].message.content
+    
     logger.info(content)
     
     status_url = os.getenv("MASTODON_URL") + "/api/v1/statuses"
@@ -104,17 +118,20 @@ def run(event, context):
     
     files = {
         'file': open('./output.png', 'rb'),
-        'description': f"Image of the Japanese character {unicode_char}, with numbers next to each stroke denoting stroke order." 
+    }
+    
+    data = {
+        'description': f"Image of the Japanese character {unicode_char}, with numbers next to each stroke denoting stroke order."  
     }
     
     # upload media
-    media_response = requests.post(media_url, headers=headers, files=files)
+    media_response = requests.post(media_url, headers=headers, files=files, data=data)
     
     media_id = None
     if media_response.status_code == 200:
         res = media_response.json()
         media_id = res["id"]
-        logger.info(f"Media uploaded successfully with ID: {media_id}")
+        logger.info(f"Media uploaded successfully {res}")
     else:
         logger.info(f"Failed to upload media: {media_response.status_code} - {media_response.text}")
     
@@ -128,9 +145,33 @@ def run(event, context):
     response = requests.post(status_url, headers=headers, json=data)
     
     # Check the response
+    status_ok = True
     if response.status_code == 200:
         logger.info("Status posted successfully!")
     else:
         logger.info(f"Failed to post status: {response.status_code} - {response.text}")
-    
+        status_ok = False
+        
+    # Create response post
+    if status_ok:
+        response_content = f"View more about the character 【{unicode_char}】here...\n\n" + jisho_link
+        
+        status_response = response.json()
+        response_data = {
+            'status': response_content,
+            'media_ids': [],
+            'visibility': 'unlisted',
+            'language': 'en',
+            'in_reply_to_id': status_response["id"]
+        }
+        logger.info(f"Status response: {status_response}")
+        reply_response = requests.post(status_url, headers=headers, json=response_data)
+        
+        if reply_response.status_code == 200:
+            logger.info("Reply Status posted successfully!")
+            reply_res_j = reply_response.json()
+            logger.info(reply_res_j)
+        else:
+            logger.info(f"Failed to post reply status: {reply_response.status_code} - {reply_response.text}")
+        
 run(0,0)
